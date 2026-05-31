60ba39cc69dc84e285190628e1e6a4cfddd1d9c1
ThitayaT26 <thityataemsara@gmail.com>
2026-05-31 23:56:28 +0700

    Update moving_average_filter.h

--- Moving Average Filter/moving_average_filter.h
+++ Moving Average Filter/moving_average_filter.h
@@ -1,1 +1,20 @@
+#ifndef MOVING_AVERAGE_FILTER_H
+#define MOVING_AVERAGE_FILTER_H
 
+#include <Arduino.h>
+
+class MovingAverageFilter {
+private:
+  int windowSize;
+  float* buffer;
+  int index;
+  float sum;
+  bool filled;
+
+public:
+  MovingAverageFilter(int size);
+  ~MovingAverageFilter();
+  float filter(float input);
+};
+
+#endif
