695ed3e5e68f5194a541e70d6d0657a00d6f4324
ThitayaT26 <thityataemsara@gmail.com>
2026-05-31 23:57:13 +0700

    Update moving_average_filter.cpp

--- Moving Average Filter/moving_average_filter.cpp
+++ Moving Average Filter/moving_average_filter.cpp
@@ -1,1 +1,28 @@
+#include "moving_average_filter.h"
 
+MovingAverageFilter::MovingAverageFilter(int size) {
+  windowSize = size;
+  buffer = new float[size];
+  index = 0;
+  sum = 0.0;
+  filled = false;
+  for (int i = 0; i < windowSize; i++) {
+    buffer[i] = 0.0;
+  }
+}
+
+MovingAverageFilter::~MovingAverageFilter() {
+  delete[] buffer;
+}
+
+float MovingAverageFilter::filter(float input) {
+  sum -= buffer[index];
+  buffer[index] = input;
+  sum += buffer[index];
+  index = (index + 1) % windowSize;
+  
+  if (index == 0) filled = true;
+  
+  int count = filled ? windowSize : index;
+  return sum / count;
+}
